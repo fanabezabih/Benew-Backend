459fb147c032d4853966e6a7184d363e6ff52a56
Fana Asmelash <fanabezabih@gmail.com>
2026-05-11 11:42:59 +0300

    edited importing file code

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,21 @@
+import "./globals.css";
+
 import { AuthProvider } from "@/context/AuthContext";
 import { ModalProvider } from "@/context/ModalContext";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
-
         <AuthProvider>
           <ModalProvider>
             {children}
           </ModalProvider>
         </AuthProvider>
-
       </body>
     </html>
   );
